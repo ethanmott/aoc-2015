30e1cee27c3b386ebe7156533293e969d218f2f9
Ethan Mott <ethanmott@gmail.com>
2020-01-20 21:53:50 -0500

    wrap up day 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -3,21 +3,26 @@ use common::files;
 fn main() {
     let bytes = files::get_file_bytes("day01.txt");
 
-    println!("{}", day01(&bytes));
+    println!("{:?}", day01(&bytes));
 }
 
-fn day01(input: &[u8]) -> i32 {
+fn day01(input: &[u8]) -> (i32, Option<i32>) {
     let mut floor: i32 = 0;
+    let mut first_basement_position: Option<i32> = None;
 
-    for c in input {
+    for (i, c) in input.iter().enumerate() {
         match c {
             b'(' => floor += 1,
             b')' => floor -= 1,
             _ => panic!("Invalid command character.")
         }
+
+        if floor == -1 && first_basement_position.is_none() {
+            first_basement_position = Some((i + 1) as i32);
+        }
     }
 
-    floor
+    (floor, first_basement_position)
 }
 
 #[cfg(test)]
@@ -26,12 +31,13 @@ mod tests {
 
     #[test]
     fn test_day01() {
-        assert_eq!(day01("".as_bytes()), 0);
-        assert_eq!(day01("(".as_bytes()), 1);
-        assert_eq!(day01(")".as_bytes()), -1);
-        assert_eq!(day01("((".as_bytes()), 2);
-        assert_eq!(day01("))".as_bytes()), -2);
-        assert_eq!(day01("()(".as_bytes()), 1);
-        assert_eq!(day01(")))))(((((".as_bytes()), 0);
+        assert_eq!(day01("".as_bytes()), (0, None));
+        assert_eq!(day01("(".as_bytes()), (1, None));
+        assert_eq!(day01(")".as_bytes()), (-1, Some(1)));
+        assert_eq!(day01("((".as_bytes()), (2, None));
+        assert_eq!(day01("))".as_bytes()), (-2, Some(1)));
+        assert_eq!(day01("()(".as_bytes()), (1, None));
+        assert_eq!(day01(")))))(((((".as_bytes()), (0, Some(1)));
+        assert_eq!(day01("()())".as_bytes()), (-1, Some(5)));
     }
 }
